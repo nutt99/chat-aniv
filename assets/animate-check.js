9662afff7e27c4e97d5249a9ffaec47da2bc68e8
nutt99 <NataGaming766@gmail.com>
2024-09-03 02:28:34 +0700

    updateee lagiii

--- assets/animate-check.js
+++ assets/animate-check.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
     const chat1 = document.getElementById("chat1");
+
+    const container_out2 = document.getElementById("container_out2")
     
     const chat21 = document.getElementById("chat2-1");
     const chat22 = document.getElementById("chat2-2");
@@ -9,6 +11,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
     chat1.addEventListener('animationend', () => {
         document.getElementById("choose_point").classList.remove("d-none")
     });
+
+    container_out2.addEventListener('animationend', () => {
+        document.getElementById("choose_point2").classList.remove("d-none");
+    })
 
     // outg2.addEventListener('animationend', () => {
     //     document.getElementById("chat2").innerHTML = '<div class="message message-incoming" id="chat2"><p>Emm.... Sebelum mulai apakah aku boleh puter musik dulu?</p><span class="timestamp">09:32</span></div>'
